48ac4258982fbbe7a030d578e241082687e78bdb
Arun Roy <arunroy2011991@gmail.com>
2023-07-31 16:05:50 +0530

    inisial

---
+++ assets/js/script.js
@@ -0,0 +1,216 @@
+const mobileMenu = document.querySelector(".navbar-collapse");
+
+// Get the toggle button element
+const toggleButton = document.querySelector(".navbar-toggler");
+
+// Function to toggle the mobile menu
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("show");
+}
+
+// Function to close the mobile menu
+function closeMobileMenu() {
+  mobileMenu.classList.remove("show");
+}
+
+// Event listener for the toggle button
+toggleButton.addEventListener("click", toggleMobileMenu);
+
+// Event listener for clicks outside the menu
+document.addEventListener("click", function (event) {
+  const targetElement = event.target;
+  if (targetElement !== toggleButton && !mobileMenu.contains(targetElement)) {
+    closeMobileMenu();
+  }
+});
+
+// header color function
+function setHeaderColor() {
+  const header = document.getElementById("header");
+  const pageTitle = document.title;
+  const navlinks = document.querySelectorAll(".navbar-nav li a");
+  const active = document.querySelector(".active");
+  console.log(active);
+
+  // Replace 'Home' with the title of your home page
+  const isHomePage = pageTitle === "Home";
+
+  if (isHomePage) {
+    header.style.backgroundColor = "transparent";
+    $(".navbar-toggler").on("click", function () {
+      $(".navbar").toggleClass("bg-white");
+      navlinks.forEach((link) => {
+        link.style.color = "black";
+      });
+    });
+  } else {
+    header.style.backgroundColor = "white";
+    // Loop through each navlink and set the color to black
+    navlinks.forEach((link) => {
+      link.style.color = "black";
+      active.style.color = "#c070ff";
+    });
+  }
+}
+
+// Call the function when the page loads
+window.onload = setHeaderColor;
+
+$(document).ready(function () {
+  // homepage mobile nav toggle
+
+  // gallery slider is here
+  $("#gallery_slider1").slick({
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          // dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+
+  $("#gallery_slider2").slick({
+    dots: false,
+    rtl: true,
+    autoplay: true,
+    arrows: false,
+    infinite: true,
+    speed: 200,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          // dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+
+  // Add custom buttons
+  $(".price-prev").click(function () {
+    $(".pricing-slider").slick("slickPrev");
+  });
+
+  $(".price-next").click(function () {
+    $(".pricing-slider").slick("slickNext");
+  });
+
+  $(".pricing-slider").slick({
+    dots: false,
+    autoplay: false,
+    infinite: true,
+    speed: 200,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          // dots: fa,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
+  $(".flip-card-slider").slick({
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    speed: 200,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          // dots: fa,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
